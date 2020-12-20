c7a249dfb13869c34f373a3a45c5a2cf6862a914
Kartikey Singh <59006007+kartikeysingh6@users.noreply.github.com>
2020-12-20 10:20:16 +0530

    Added destroy window and cap don't show

--- VdoAndWebcam/webcam.py
+++ VdoAndWebcam/webcam.py
@@ -1,7 +1,7 @@
 import cv2
 print("System up and running...")
 
-vdo=cv2.VideoCapture(0)
+vdo=cv2.VideoCapture(0, cv2.CAP_DSHOW)	#doesn't shows warning
 vdo.set(3,640)	#width id is 3
 vdo.set(4,480)	#height id is 4
 vdo.set(10,100)	#brightness id is 10
@@ -9,5 +9,7 @@ vdo.set(10,100)	#brightness id is 10
 while True:	#video is nothing but a sequence of images
 	success, img = vdo.read()
 	cv2.imshow("Video output ",img)
+	
 	if cv2.waitKey(1) & 0xFF ==ord('e'):	#e to exit
+		cv2.destroyAllWindows()
 		break
